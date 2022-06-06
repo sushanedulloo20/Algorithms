e421957239b11a0f5cca122b3bb4f8d37256b661
Mahir <96955190+mahir-yadav@users.noreply.github.com>
2022-06-06 10:20:51 +0530

    update BST

---
+++ countnode_bst.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+struct node* createnode(int data) {
+	struct node* n;
+	n = (struct node*)malloc(sizeof(struct node));
+	n->data = data;
+	n->left = NULL;
+	n->right = NULL;
+	return n;
+}
+int count(struct node* node) {
+	if (node == NULL) {
+		return 0;
+	} else {
+		return 1 + count(node->left) + count(node->right);
+	}
+}
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input5.txt", "r", stdin);
+	freopen("output5.txt", "w", stdout);
+#endif
+	struct node *p = createnode(4);
+	struct node *p1 = createnode(1);
+	struct node *p2 = createnode(6);
+	struct node *p3 = createnode(5);
+	struct node *p4 = createnode(2);
+	p->left = p1;
+	p->right = p2;
+	p1->left = p3;
+	p1->right = p4;
+	printf("%d", count(p));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
